e0dbd2bfb2027852234f27a8941c818b84eb0417
asyaefudinz <asyaefudinz25@gmail.com>
2021-03-01 08:29:46 +0700

    UAS SMT 1

---
+++ Take_Home_Pay_UAS.cpp
@@ -0,0 +1,35 @@
+//Menghitung Take Home Pay (Gaji Bersih) 
+#include<iostream>
+#include<string>
+const float persentunjangan = 0.25;
+const float persenpajak = 0.1;
+
+void line()
+{
+	std::cout<<"================================\n"<<std::endl;
+}
+
+int main()
+{
+	std::string namakaryawan;
+	int gajipokok, gajibersih, pajak, tunjangan;
+	
+	std::cout<<"::::::::: Menghitung Take Home Pay Karyawan :::::::::\n"<<std::endl;
+	
+	std::cout<<"Masukan Nama Karyawan : ";
+	getline(std::cin,namakaryawan);
+	
+	std::cout<<"Masukan Gaji Pokok : Rp. ";
+	std::cin>>gajipokok;
+	line();
+	
+	tunjangan = persentunjangan * gajipokok;
+	pajak = persenpajak * gajipokok;
+	gajibersih = gajipokok + tunjangan - pajak;
+	
+	std::cout<<"  Berikut gaji bersih yang di terima : "<<std::endl;
+	std::cout<<"    Nama Karyawan : "<<namakaryawan<<std::endl;
+	std::cout<<"    Gaji Bersih   : Rp. "<<gajibersih<<std::endl;
+	
+	return 0;
+}
