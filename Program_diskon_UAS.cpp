f09d1dbd3c6c5f35f587511c501a5547d8689a8f
asyaefudinz <asyaefudinz25@gmail.com>
2021-03-01 08:48:49 +0700

    UAS SMT 1

--- Program_diskon_UAS.cpp
+++ Program_diskon_UAS.cpp
@@ -7,9 +7,9 @@ int main ()
 	
 	float diskon0, diskon1, diskon2, diskon3;
 	int totalbelanja, totalbayar, diskon;
-	diskon0 = 0;
-	diskon1 = 0.1;
-	diskon2 = 0.125;
+	diskon0 = 0.05;
+	diskon1 = 0.075;
+	diskon2 = 0.1;
 	diskon3 = 0.15;
 	
 	cout << "Masukan Total Belanja : Rp. ";
@@ -19,19 +19,19 @@ int main ()
 	if (totalbelanja <= 25000){
 		diskon = totalbelanja * diskon0;
 		totalbayar = totalbelanja - diskon;
-		cout << "Diskon 0%		: Rp. " << diskon <<endl;
+		cout << "Diskon 5%		: Rp. " << diskon <<endl;
 		cout << "Total Pembayaran	: Rp. " << totalbayar <<endl<<endl;
 					
 	} else if (totalbelanja <= 50000){
 		diskon = totalbelanja * diskon1;
 		totalbayar = totalbelanja - diskon;
-		cout << "Diskon 10%		: Rp. " << diskon <<endl;
+		cout << "Diskon 7.5%		: Rp. " << diskon <<endl;
 		cout << "Total Pembayaran	: Rp. " << totalbayar <<endl<<endl;
 			
 	} else if (totalbelanja <= 100000){
 		diskon = totalbelanja * diskon2;
 		totalbayar = totalbelanja - diskon;
-		cout << "Diskon 12.5%		: Rp. " << diskon <<endl;
+		cout << "Diskon 10%		: Rp. " << diskon <<endl;
 		cout << "Total Pembayaran	: Rp. " << totalbayar <<endl<<endl;
 		 	
 	} else if (totalbelanja > 100000){
